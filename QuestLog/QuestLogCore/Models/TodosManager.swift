c2ad3fb15f7fc4234624ce1b9fb4384a1098e71c
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-18 16:13:54 -0700

    feat: add TodosManager

---
+++ QuestLog/QuestLogCore/Models/TodosManager.swift
@@ -0,0 +1,42 @@
+//
+//  TodosManager.swift
+//  QuestLogCore
+//
+//  Created by Jonathan Sweeney on 5/18/26.
+//
+
+import Foundation
+
+/// Manages the collection of todos (quests), including adding, listing, toggling, and deleting.
+public class TodosManager {
+    public var todos: [Todo] = []
+
+    public init() {}
+
+    /// Adds a new todo (quest item) with the given title
+    public func add(title: String) {
+        let todo = Todo(title: title)
+        todos.append(todo)
+    }
+
+    /// Fetches all todos (quests)
+    public func fetchAll() -> [Todo]{
+        return todos
+    }
+
+    /// Toggles the completions status of task by its ID
+    public func toggle(id: UUID) {
+        guard let index = todos.firstIndex(where: { $0.id == id }) else { return }
+        todos[index].isDone.toggle()
+    }
+
+    /// Delete a todo item by its ID
+    public func delete(id: UUID) {
+        todos.removeAll(where: { $0.id == id })
+    }
+
+    /// Delte all todo items
+    public func deleteAll() {
+        todos.removeAll()
+    }
+}
