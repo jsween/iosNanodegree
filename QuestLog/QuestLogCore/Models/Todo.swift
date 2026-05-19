420d8a1ee51b5eccc72efafa5dd67a407b106577
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-18 18:29:35 -0700

    feat: Add Codable, Equatable, and CustomStringConvertible to Todo

--- QuestLog/QuestLogCore/Models/Todo.swift
+++ QuestLog/QuestLogCore/Models/Todo.swift
@@ -8,12 +8,19 @@
 import Foundation
 
 /// A single todo item with a unique ID, tiltle, and completion status
-public struct Todo: Equatable {
-    public let id: UUID = UUID()
+public struct Todo: Codable, CustomStringConvertible, Equatable {
+    public let id: UUID
     public var title: String
     public var isDone: Bool = false
 
-    public init(title: String) {
+    public var description: String {
+        let status = isDone ? "✅" : "⬜"
+        return "\(status) \(title)"
+    }
+
+    public init(title: String, isDone: Bool = false) {
+        self.id = UUID()
         self.title = title.capitalized
+        self.isDone = isDone
     }
 }
