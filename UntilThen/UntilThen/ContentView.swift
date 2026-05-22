52ae245141a2a3db33f1ab079ed3cf880a74b3f3
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-21 19:42:18 -0700

    feat: create project 2

---
+++ UntilThen/UntilThen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UntilThen
+//
+//  Created by Jonathan Sweeney on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UntilThen/UntilThen/UntilThenApp.swift
@@ -0,0 +1,17 @@
+//
+//  UntilThenApp.swift
+//  UntilThen
+//
+//  Created by Jonathan Sweeney on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct UntilThenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
