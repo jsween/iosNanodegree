a2624c06aaa01563af3c3a084454185021232ec7
Jon Sweeney <jonsweeney26@gmail.com>
2026-06-08 11:51:11 -0700

    feat: update Ingredients to SwiftUI

--- SwiftBites_Starter/SwiftBites/Ingredients/IngredientForm.swift
+++ SwiftBites_Starter/SwiftBites/Ingredients/IngredientForm.swift
@@ -1,87 +1,84 @@
 import SwiftUI
 
 struct IngredientForm: View {
-  enum Mode: Hashable {
-    case add
-    case edit(MockIngredient)
-  }
-
-  var mode: Mode
-
-  init(mode: Mode) {
-    self.mode = mode
-    switch mode {
-    case .add:
-      _name = .init(initialValue: "")
-      title = "Add Ingredient"
-    case .edit(let ingredient):
-      _name = .init(initialValue: ingredient.name)
-      title = "Edit \(ingredient.name)"
+    enum Mode: Hashable {
+        case add
+        case edit(Ingredient)
     }
-  }
-
-  private let title: String
-  @State private var name: String
-  @State private var error: Error?
-  @Environment(\.storage) private var storage
-  @Environment(\.dismiss) private var dismiss
-  @FocusState private var isNameFocused: Bool
-
-  // MARK: - Body
-
-  var body: some View {
-    Form {
-      Section {
-        TextField("Name", text: $name)
-          .focused($isNameFocused)
-      }
-      if case .edit(let ingredient) = mode {
-        Button(
-          role: .destructive,
-          action: {
-            delete(ingredient: ingredient)
-          },
-          label: {
-            Text("Delete Ingredient")
-              .frame(maxWidth: .infinity, alignment: .center)
-          }
-        )
-      }
+    
+    var mode: Mode
+    
+    init(mode: Mode) {
+        self.mode = mode
+        switch mode {
+        case .add:
+            _name = .init(initialValue: "")
+            title = "Add Ingredient"
+        case .edit(let ingredient):
+            _name = .init(initialValue: ingredient.name)
+            title = "Edit \(ingredient.name)"
+        }
     }
-    .onAppear {
-      isNameFocused = true
+    
+    private let title: String
+    @State private var name: String
+    @State private var error: Error?
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isNameFocused: Bool
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $name)
+                    .focused($isNameFocused)
+            }
+            if case .edit(let ingredient) = mode {
+                Button(
+                    role: .destructive,
+                    action: {
+                        delete(ingredient: ingredient)
+                    },
+                    label: {
+                        Text("Delete Ingredient")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                )
+            }
+        }
+        .onAppear {
+            isNameFocused = true
+        }
+        .onSubmit {
+            save()
+        }
+        .navigationTitle(title)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save", action: save)
+                    .disabled(name.isEmpty)
+            }
+        }
     }
-    .onSubmit {
-      save()
+    
+    // MARK: - Data
+    
+    private func delete(ingredient: Ingredient) {
+        context.delete(ingredient)
+        dismiss()
     }
-    .navigationTitle(title)
-    .navigationBarTitleDisplayMode(.inline)
-    .toolbar {
-      ToolbarItem(placement: .topBarTrailing) {
-        Button("Save", action: save)
-          .disabled(name.isEmpty)
-      }
+    
+    private func save() {
+        switch mode {
+        case .add:
+            let ingredient = Ingredient(name: name)
+            context.insert(ingredient)
+        case .edit(let ingredient):
+            ingredient.name = name
+        }
+        dismiss()
     }
-  }
-
-  // MARK: - Data
-
-  private func delete(ingredient: MockIngredient) {
-    storage.deleteIngredient(id: ingredient.id)
-    dismiss()
-  }
-
-  private func save() {
-    do {
-      switch mode {
-      case .add:
-        try storage.addIngredient(name: name)
-      case .edit(let ingredient):
-        try storage.updateIngredient(id: ingredient.id, name: name)
-      }
-      dismiss()
-    } catch {
-      self.error = error
-    }
-  }
 }
--- SwiftBites_Starter/SwiftBites/Ingredients/IngredientsView.swift
+++ SwiftBites_Starter/SwiftBites/Ingredients/IngredientsView.swift
@@ -1,125 +1,138 @@
 import SwiftUI
+import SwiftData
 
 struct IngredientsView: View {
-  typealias Selection = (MockIngredient) -> Void
+    typealias Selection = (Ingredient) -> Void
 
-  let selection: Selection?
+    let selection: Selection?
 
-  init(selection: Selection? = nil) {
-    self.selection = selection
-  }
+    init(selection: Selection? = nil) {
+        self.selection = selection
+    }
 
-  @Environment(\.storage) private var storage
-  @Environment(\.dismiss) private var dismiss
-  @State private var query = ""
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    @State private var query = ""
 
-  // MARK: - Body
+    @Query private var ingredients: [Ingredient]
 
-  var body: some View {
-    NavigationStack {
-      content
-        .navigationTitle("Ingredients")
-        .toolbar {
-          if !storage.ingredients.isEmpty {
-            NavigationLink(value: IngredientForm.Mode.add) {
-              Label("Add", systemImage: "plus")
-            }
-          }
-        }
-        .navigationDestination(for: IngredientForm.Mode.self) { mode in
-          IngredientForm(mode: mode)
+    // MARK: - Body
+
+    var body: some View {
+        NavigationStack {
+            content
+                .navigationTitle("Ingredients")
+                .toolbar {
+                    if !ingredients.isEmpty {
+                        NavigationLink(value: IngredientForm.Mode.add) {
+                            Label("Add", systemImage: "plus")
+                        }
+                    }
+                }
+                .navigationDestination(for: IngredientForm.Mode.self) { mode in
+                    IngredientForm(mode: mode)
+                }
         }
     }
-  }
 
-  // MARK: - Views
+    // MARK: - Views
 
-  @ViewBuilder
-  private var content: some View {
-    if storage.ingredients.isEmpty {
-      empty
-    } else {
-      list(for: storage.ingredients.filter {
-        if query.isEmpty {
-          return true
+    @ViewBuilder
+    private var content: some View {
+        if ingredients.isEmpty && query.isEmpty {
+            empty
         } else {
-          return $0.name.localizedStandardContains(query)
+            list
         }
-      })
     }
-  }
 
-  private var empty: some View {
-    ContentUnavailableView(
-      label: {
-        Label("No Ingredients", systemImage: "list.clipboard")
-      },
-      description: {
-        Text("Ingredients you add will appear here.")
-      },
-      actions: {
-        NavigationLink("Add Ingredient", value: IngredientForm.Mode.add)
-          .buttonBorderShape(.roundedRectangle)
-          .buttonStyle(.borderedProminent)
-      }
-    )
-  }
+    private var empty: some View {
+        ContentUnavailableView(
+            label: {
+                Label("No Ingredients", systemImage: "list.clipboard")
+            },
+            description: {
+                Text("Ingredients you add will appear here.")
+            },
+            actions: {
+                NavigationLink("Add Ingredient", value: IngredientForm.Mode.add)
+                    .buttonBorderShape(.roundedRectangle)
+                    .buttonStyle(.borderedProminent)
+            }
+        )
+    }
 
-  private var noResults: some View {
-    ContentUnavailableView(
-      label: {
-        Text("Couldn't find \"\(query)\"")
-      }
-    )
-    .listRowSeparator(.hidden)
-  }
+    private var noResults: some View {
+        ContentUnavailableView(
+            label: {
+                Text("Couldn't find \"\(query)\"")
+            }
+        )
+        .listRowSeparator(.hidden)
+    }
 
-  private func list(for ingredients: [MockIngredient]) -> some View {
-    List {
-      if ingredients.isEmpty {
-        noResults
-      } else {
-        ForEach(ingredients) { ingredient in
-          row(for: ingredient)
-            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-              Button("Delete", systemImage: "trash", role: .destructive) {
-                delete(ingredient: ingredient)
-              }
+    private var list: some View {
+        List {
+            if ingredients.isEmpty {
+                noResults
+            } else {
+                ForEach(ingredients) { ingredient in
+                    row(for: ingredient)
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button("Delete", systemImage: "trash", role: .destructive) {
+                                delete(ingredient: ingredient)
+                            }
+                        }
+                }
             }
         }
-      }
-    }
-    .searchable(text: $query)
-    .listStyle(.plain)
-  }
-
-  @ViewBuilder
-  private func row(for ingredient: MockIngredient) -> some View {
-    if let selection {
-      Button(
-        action: {
-          selection(ingredient)
-          dismiss()
-        },
-        label: {
-          title(for: ingredient)
+        .searchable(text: $query)
+        .listStyle(.plain)
+        .onChange(of: query) {
+            filterIngredients()
         }
-      )
-    } else {
-      NavigationLink(value: IngredientForm.Mode.edit(ingredient)) {
-        title(for: ingredient)
-      }
     }
-  }
 
-  private func title(for ingredient: MockIngredient) -> some View {
-    Text(ingredient.name)
-      .font(.title3)
-  }
+    @ViewBuilder
+    private func row(for ingredient: Ingredient) -> some View {
+        if let selection {
+            Button(
+                action: {
+                    selection(ingredient)
+                    dismiss()
+                },
+                label: {
+                    title(for: ingredient)
+                }
+            )
+        } else {
+            NavigationLink(value: IngredientForm.Mode.edit(ingredient)) {
+                title(for: ingredient)
+            }
+        }
+    }
 
-  // MARK: - Data
+    private func title(for ingredient: Ingredient) -> some View {
+        Text(ingredient.name)
+            .font(.title3)
+    }
 
-  private func delete(ingredient: MockIngredient) {
-    storage.deleteIngredient(id: ingredient.id)
-  }
+    // MARK: - Data
+
+    private func delete(ingredient: Ingredient) {
+        context.delete(ingredient)
+    }
+
+    private func filterIngredients() {
+        do {
+            let predicate = #Predicate<Ingredient> { ingredient in
+                query.isEmpty ? true : ingredient.name.localizedStandardContains(query)
+            }
+            let descriptor = FetchDescriptor(predicate: predicate)
+            let filtered = try context.fetch(descriptor)
+            _ = filtered
+        } catch {
+            print("Filter error: \(error)")
+        }
+    }
 }
