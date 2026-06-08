8442d133aeee6a511dcf6360e9bd1485bf50463d
Jon Sweeney <jonsweeney26@gmail.com>
2026-06-08 11:04:17 -0700

    refactor: replace mock models with new models

---
+++ SwiftBites_Starter/SwiftBites/Models/Models.swift
@@ -0,0 +1,65 @@
+//
+//  Models.swift
+//  SwiftBites
+//
+//  Created by Jonathan Sweeney on 6/8/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Category {
+    var name: String
+
+    @Relationship(deleteRule: .nullify, inverse: \Recipe.category)
+    var recipes: [Recipe] = []
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+@Model
+final class Recipe {
+    var name: String
+    var summary: String
+    var serving: Int
+    var time: Int
+    var instructions: String
+    var imageData: Data?
+    var category: Category?
+
+    @Relationship(deleteRule: .cascade)
+    var ingredients: [RecipeIngredient] = []
+
+    init(name: String, summary: String = "", serving: Int = 1, time: Int = 20, instructions: String = "", imageData: Data? = nil, category: Category? = nil) {
+        self.name = name
+        self.summary = summary
+        self.serving = serving
+        self.time = time
+        self.instructions = instructions
+        self.imageData = imageData
+        self.category = category
+    }
+}
+
+@Model
+final class RecipeIngredient {
+    var quantity: String
+    var ingredient: Ingredient?
+
+    init(quantity: String, ingredient: Ingredient) {
+        self.quantity = quantity
+        self.ingredient = ingredient
+    }
+}
+
+@Model
+final class Ingredient {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
